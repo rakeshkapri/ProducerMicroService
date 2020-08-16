0eed074563afd912b67c119f02ef6213db1b0487
Rakesh Kapri <rakeshkapri@gmail.com>
2020-08-16 18:20:47 +0530

    Added skeleton producer MS code

---
+++ src/main/java/com/producer/ProducerMicroService/ProducerMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.producer.ProducerMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerMicroServiceApplication.class, args);
+	}
+
+}
