b07f110a90b390676c9f88c3b8aa995025a017e9
Rakesh Kapri <rakeshkapri@gmail.com>
2020-08-16 18:40:11 +0530

    Added eureka service integration

--- src/main/java/com/producer/ProducerMicroService/ProducerMicroServiceApplication.java
+++ src/main/java/com/producer/ProducerMicroService/ProducerMicroServiceApplication.java
@@ -2,8 +2,10 @@ package com.producer.ProducerMicroService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProducerMicroServiceApplication {
 
 	public static void main(String[] args) {
